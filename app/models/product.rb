5200e4bc36b84c8391dd7ae6b21c7036810ffe38
Radosław Stolarski <stolarz92@gmail.com>
2015-06-01 23:03:09 +0200

    Ex.4, product model validations, average rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :title, :description, :price, presence: true
+  validates :price,
+            :numericality => true,
+            :format => { :with => /\A\d{1,4}(\.\d{0,2})?\z/ }
+
+  def average_rating
+    self.reviews.average(:rating)
+  end
 end
