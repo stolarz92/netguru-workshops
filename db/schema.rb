fe4cc0ce4d18debe7c214c2579e7644d50c7cc36
Radosław Stolarski <stolarz92@gmail.com>
2015-06-11 16:36:50 +0200

    Ex.16, add seeds with users, category, products and reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150601191752) do
+ActiveRecord::Schema.define(version: 20150611132220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20150601191752) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,82 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'ffaker'
+def create_admin
+  User.create!(
+      email: 'admin@example.com',
+      firstname: 'admin',
+      lastname: 'lastname',
+      password: 'password',
+      password_confirmation: 'password',
+      admin: true
+  )
+end
+
+def create_users
+  5.times do
+    email = Faker::Internet.email
+    firstname = Faker::Name.first_name
+    lastname = Faker::Name.last_name
+    password = 'password'
+    password_confirmation = 'password'
+
+    User.create!(
+        email: email,
+        firstname: firstname,
+        lastname: lastname,
+        password: password,
+        password_confirmation: password_confirmation
+    )
+  end
+end
+
+def create_category
+  Category.create(name: 'Misc')
+end
+
+def create_products
+  category = Category.first
+  users = User.all[1..-1]
+
+  users.each do |user|
+    5.times do
+      title = Faker::Product.product
+      description = Faker::Lorem.sentence
+      price = [10,20,35,50,100].sample
+
+      category.products.create(
+          title: title,
+          description: description,
+          price: price,
+          category_id: category,
+          user_id: user.id
+      )
+    end
+  end
+end
+
+def create_reviews
+  users = User.all
+
+  users.each do |user|
+    5.times do
+      rating = (1..5).to_a.sample
+      content = Faker::Lorem.sentence(5)
+      product_id = (1..25).to_a.sample
+
+      user.reviews.create!(
+          content: content,
+          rating: rating,
+          product_id: product_id,
+      )
+    end
+  end
+end
+
+create_admin
+create_users
+create_category
+create_products
+create_reviews
+
